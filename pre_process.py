ce77bdda34f9c4bf60b9fc9c4afb2c333137b99f
chouxi <qizheng1993hit@gmail.com>
2017-04-25 12:23:50 -0400

    First version part 1

---
+++ pre_process.py
@@ -0,0 +1,28 @@
+import numpy as np
+from skimage import color, exposure, transform
+from skimage import io
+
+NUM_CLASSES = 43
+IMG_SIZE = 48
+
+def preprocess_img(img):
+    # Histogram normalization in v channel
+    hsv = color.rgb2hsv(img)
+    hsv[:,:,2] = exposure.equalize_hist(hsv[:,:,2])
+    img = color.hsv2rgb(hsv)
+
+    # central square crop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0]//2, img.shape[1]//2
+    img = img[centre[0]-min_side//2:centre[0]+min_side//2,
+              centre[1]-min_side//2:centre[1]+min_side//2,
+              :]
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img,-1)
+
+    return img
+
---
+++ test_baseline.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import os
+import pre_process
+import cnn_model
+
+test = pd.read_csv('GT-final_test.csv',sep=';')
+
+model = cnn_model.cnn_model()
+model.load_weights("model.h5")
+# Load test dataset
+X_test = []
+y_test = []
+i = 0
+for file_name, class_id  in zip(list(test['Filename']), list(test['ClassId'])):
+    img_path = os.path.join('GTSRB/Final_Test/Images/',file_name)
+    X_test.append(pre_process.preprocess_img(io.imread(img_path)))
+    y_test.append(class_id)
+    
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+# predict and evaluate
+y_pred = model.predict_classes(X_test)
+acc = np.sum(y_pred==y_test)/np.size(y_pred)
+print("Test accuracy = {}".format(acc))
